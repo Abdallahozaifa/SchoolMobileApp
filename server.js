a73c8a10e6feab574af54c6460de59a29aa015cc
Hozaifa Abdalla <abdallahozaifa19527@gmail.com>
2017-08-13 12:55:29 -0400

    Added socket.io with client

---
+++ server.js
@@ -0,0 +1,133 @@
+/**
+ * @fileOverview Server file that handles all the requests
+ * @author Hozaifa Abdalla
+ * @version 1.0.0
+ * @module server js
+ */
+
+/* LIBRARY IMPORTS */
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var bodyParser = require("body-parser");
+var request = require('request');
+var timer = require('./server_modules/timer');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+/* Express Body Parser*/
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res, next) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(client) {
+    console.log('Client connected...');
+
+    client.on('join', function(data) {
+        console.log(data);
+        client.emit('messages', 'Hello from server');
+    });
+});
+
+var accessToken = '1050~xJKSKyi7ZrBkbaOqIxy4X0gyfXDQX7ujmCLm7OwFtlmgFvAOgWlcH6zYVah1nex4';
+// app.use('/css', express.static('css'));
+
+//curl -H "Authorization: Bearer 1050~xJKSKyi7ZrBkbaOqIxy4X0gyfXDQX7ujmCLm7OwFtlmgFvAOgWlcH6zYVah1nex4" https://psu.instructure.com/api/v1/courses
+var processRequest = function(url, callback) {
+
+    var options = {
+        url: url,
+        headers: {
+            'Authorization': 'Bearer 1050~xJKSKyi7ZrBkbaOqIxy4X0gyfXDQX7ujmCLm7OwFtlmgFvAOgWlcH6zYVah1nex4'
+        }
+    };
+
+    request(options, function(err, res, body) {
+        if (!err && res.statusCode == 200) {
+            callback(body);
+        }
+        else {
+            callback(res);
+        }
+    });
+};
+
+var getCourses = function(callback) {
+    processRequest('https://psu.instructure.com/api/v1/courses', function(courseArr) {
+        var courseArr = JSON.parse(courseArr);
+        callback(courseArr);
+    });
+};
+
+
+/**/
+var getClassMaterial = function(url, callback) {
+    processRequest(url, function(assignments) {
+        callback(assignments);
+    });
+};
+
+
+// timer.setTimer("2017-02", time.timeOfDay);
+// /api/v1/users/:user_id/courses/:course_id/assignments
+/* Obtains all the courses */
+// getCourses(function(courseArr) {
+//     /* Iterates through all the courses */
+//     courseArr.forEach(function(course) {
+//         var courseID = course.id;
+//         /* Obtains all the assignments */
+//         getClassMaterial('https://psu.instructure.com/api/v1/courses/' + courseID + '/assignments/', function(assignments) {
+//             console.log("-------------" + course.name + "---------------");
+//             var assignments = JSON.parse(assignments);
+//             /* Iterates through all the assignments */
+//             assignments.forEach(function(assignment) {
+//                 console.log("Assignment Name: %s", assignment.name);
+//                 var time = timer.convertTimeFormat(assignment["due_at"]);
+//                 if (time != null) {
+//                     console.log("Assignment Due Date: <%s, %s>", time.date, time.timeOfDay);
+//                     timer.setTimer(time.date, time.timeOfDay);
+//                 }
+//             });
+//         });
+//         console.log("\n");
+//     });
+// });
+
+
+/**
+ * GET Request handler for '/'
+ * Main Page of app.
+ * @function
+ * @param {string} - The string url for the handler
+ * @param {callback} - The callback function
+ * @module server js
+ */
+// app.get('/about', function(req, res) {
+//     fs.readFile('about.html', 'utf8', function(err, data) {
+//         if (!err) res.send(data);
+//         else return console.log(err);
+//     });
+// });
+
+/**
+ * Server function for listening for request on the port 8080
+ * @function
+ * @param {string} - The string url for the port
+ * @param {callback} - The callback function
+ * @module beacon js
+ */
+// var server = app.listen(process.env.PORT || '8080', '0.0.0.0', function() {
+//     var host = server.address().address;
+//     var port = server.address().port;
+//     console.log('running at http://' + host + ':' + port + "/");
+// });
+server.listen(process.env.PORT || '8080', '0.0.0.0', function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('running at http://' + host + ':' + port + "/");
+});
---
+++ server_modules/timer.js
@@ -0,0 +1,56 @@
+/**
+ * @fileOverview Timer module.
+ * @author Hozaifa Abdalla
+ * @version 1.0.0
+ * @module Timer js
+ */
+
+/****************************************
+ *             TIMER MODULE             *
+ ****************************************/
+(function() {
+    var setTimer = function(date, time, notificationTime) {
+        var countDownDate = new Date(date + " " + time).getTime();
+
+        // Update the count down every 1 second
+        var x = setInterval(function() {
+
+            // Get todays date and time
+            var now = new Date().getTime();
+
+            // Find the distance between now an the count down date
+            var distance = countDownDate - now;
+
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            // If the count down is over, write some text 
+            if (distance < 0) {
+                clearInterval(x);
+                console.log("Time to send Notification");
+            }
+            else {
+                console.log("Current Time: %s:%s:%s", new Date().getHours(), new Date().getMinutes(), new Date().getSeconds());
+                // Output the result in an element with id="demo"
+                console.log(days + "d " + hours + "h " + minutes + "m " + seconds + "s ");
+            }
+        }, notificationTime);
+    };
+
+    var convertTimeFormat = function(timeStr) {
+        if (timeStr == null)
+            return;
+        var timeArr = timeStr.split("T");
+        var date = timeArr[0];
+        var timeOfDay = timeArr[1].split("Z")[0];
+        return {
+            date: date,
+            timeOfDay: timeOfDay
+        };
+    };
+    exports.setTimer = setTimer;
+    exports.convertTimeFormat = convertTimeFormat;
+}).call(this);
